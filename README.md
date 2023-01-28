2ac07728cfe977acd22ad1d65f666f8825584c05
Ardiegon <oskbsz@gmail.com>
2023-01-28 05:08:43 +0100

    folder structure

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# TransformerSegmentation
+# Twitter2Money
+
+## Literature 
+
+Jupyters
+1. https://uvadlc-notebooks.readthedocs.io/en/latest/tutorial_notebooks/tutorial6/Transformers_and_MHAttention.html
+2. https://uvadlc-notebooks.readthedocs.io/en/latest/tutorial_notebooks/tutorial15/Vision_Transformer.html
+
+Blogs
+1. https://ai.googleblog.com/2017/08/transformer-novel-neural-network.html
+2. http://jalammar.github.io/illustrated-transformer/
+3. https://lilianweng.github.io/lil-log/2018/06/24/attention-attention.html
+4. https://towardsdatascience.com/illustrated-self-attention-2d627e33b20a
+5. https://lilianweng.github.io/lil-log/2020/04/07/the-transformer-family.html
+
+Utils
+1. https://realpython.com/lessons/module-search-path/
---
+++ configure.py
@@ -0,0 +1,40 @@
+import os
+import site
+
+
+def set_path_via_sys():
+    root_path = os.path.join(os.getcwd())
+    src_path = os.path.join(os.getcwd(), "src")
+    FIND_MY_PACKAGES = (
+        f'import site\nsite.addsitedir("{root_path}")\nsite.addsitedir("{src_path}")'.replace('\\','\\\\')
+    )
+    try:
+        os.makedirs(site.USER_SITE)
+    except:
+        pass
+    filename = os.path.join(site.USER_SITE, "sitecustomize.py")
+    with open(filename, "w") as outfile:
+        print(FIND_MY_PACKAGES, file=outfile)
+        print(f'Path set to "{root_path}" and "{src_path}"')
+
+def handle_folders_structure():
+    create_folder("checkpoints")
+    create_folder("assets")
+    create_folder("logs")
+    create_folder("temp")
+    create_folder("results")
+
+def create_folder(name):
+    try:
+        os.mkdir(name)
+    except:
+        pass
+
+
+def main():
+    set_path_via_sys()
+    handle_folders_structure()
+
+
+if __name__ == "__main__":
+    main()
